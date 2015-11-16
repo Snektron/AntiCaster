d47f133268003915cd6c1b22b3454969b4453cfa
RobinDeWalvis <voetterrb1a@gmail.com>
2015-11-16 19:36:49 +0100

    Better input

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	window = SDL_CreateWindow("Raycaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+	window = SDL_CreateWindow("AntiCaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 			640, 480, SDL_WINDOW_OPENGL);
 
 	if (window == NULL)
@@ -28,9 +28,8 @@ int main(int argc, char* argv[])
 	SDL_Event e;
 	SDL_Surface* sf = SDL_GetWindowSurface(window);
 
-	SDL_SetWindowGrab(window, SDL_TRUE);
-	SDL_ShowCursor(0);
-	SDL_WarpMouseInWindow(window, sf->w/2, sf->h/2);
+	int pressed = 0;
+
 	game_init();
 
 	int tpsGameTick = SDL_GetTicks(), tpsLoops;
@@ -47,11 +46,33 @@ int main(int argc, char* argv[])
 
 		while(SDL_GetTicks() > tpsGameTick && tpsLoops < MAX_FRAMESKIP)
 		{
-			int dmx, dmy;
-			SDL_GetMouseState(&dmx, &dmy);
-			if (game_update(dmx - sf->w/2, dmy - sf->h/2))
+			SDL_PumpEvents();
+			int dmx = 0, dmy = 0, state = SDL_GetMouseState(&dmx, &dmy);
+			dmx -= sf->w/2;
+			dmy -= sf->h/2;
+			if (!pressed)
+			{
+				if (state & SDL_BUTTON(SDL_BUTTON_LEFT))
+				{
+					pressed = 1;
+					SDL_SetWindowGrab(window, SDL_TRUE);
+					SDL_ShowCursor(0);
+					SDL_WarpMouseInWindow(window, sf->w/2, sf->h/2);
+				}
+				dmx = 0;
+				dmy = 0;
+			}else if (!(state & SDL_BUTTON(SDL_BUTTON_LEFT)))
+			{
+				SDL_ShowCursor(1);
+				SDL_SetWindowGrab(window, SDL_FALSE);
+				SDL_FreeSurface(sf);
+				pressed = 0;
+			}
+
+			if (game_update(dmx, dmy))
 				quit = 1;
-			SDL_WarpMouseInWindow(window, sf->w/2, sf->h/2);
+			if (pressed)
+				SDL_WarpMouseInWindow(window, sf->w/2, sf->h/2);
 			tpsGameTick += 1000/TPS;
 			tpsLoops++;
 		}
